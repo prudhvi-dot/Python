85a5d35baf7e447a9c3207901b196341cb3672a0
prudhvi-dot <112791386+prudhvi-dot@users.noreply.github.com>
2024-02-06 20:14:28 +0530

    Add files via upload

---
+++ practical1.py
@@ -0,0 +1,9 @@
+# F = (9/5)C + 32  celsius to fahrenheit
+c=float(input("enter celsius value : "))
+f=(c*(9/5)+32)
+print("fahrenheit is : ",f,"f")
+    
+#, °C = (°F - 32) × 5/9  fahrenheit to celsius
+f=int(input("enter fahrenheit value : "))
+c=((f-32)*5/9)
+print("celsius convertion is: ",c,"c")
